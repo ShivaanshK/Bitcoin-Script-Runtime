5854bdec975d8d08a1caaa00032ebd9ffb485d8b
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2024-02-22 14:49:36 -0800

    check if tx is valid or not post exec

--- main.py
+++ main.py
@@ -32,6 +32,13 @@ def main(argv: list[str]) -> int:
 			MAIN_STACK.push(instr)
 		print(MAIN_STACK)
 
+
+	if MAIN_STACK.empty() or not opcodes.convert_to_bool(MAIN_STACK.pop()):
+		print("Invalid Transaction!")
+	else:
+		print("Valid Transaction!")
+		return 0
+
 	return 0
 
 
